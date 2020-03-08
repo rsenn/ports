2cc8773fb6255ad046131d0a1339699bd41f5008
Roman <roman.l.senn@gmail.com>
2020-03-08 13:31:51 +0100

    ...

--- net/luarocks/luarocks.sh
+++ net/luarocks/luarocks.sh
@@ -1,13 +1,28 @@
 alias luarocks=_luarocks
 
 _luarocks() {
- (CMD="$1"
-  FILTER=""
+ ([ "$DEBUG" ] && set -x; unset CMD OPTS FILTER
+  while [ $# -gt 0 ]; do
+    case "$1" in
+      -*) OPTS="${OPTS:+$OPTS
+}$1" ; shift ;;
+      *) CMD="$1"; shift; break ;;
+    esac
+  done
   case "$CMD" in
     search)  FILTER="${FILTER:+$FILTER ;; } /^[-=\\s]\+\$/d ;; /^\$/d ;; /^   [^ ]/d ;; /Search results for/d ;; /Rockspecs and source rocks:/d"    ;;
   esac
-  shift
+  while [ $# -gt 0 ]; do
+    case "$1" in
+      -*) OPTS="${OPTS:+$OPTS
+}$1" ; shift ;;
+      *) break ;;
+    esac
+  done
+  set +x
+var_dump CMD OPTS FILTER
+[ $# -gt 0 ] ||  set -- ""
   for ARG; do 
-    eval "(echo 'Searching $ARG ...' 1>&2 ; set -x; command luarocks \$CMD \$ARG${FILTER:+ | sed -e '$FILTER'}) || exit \$?"
+    eval "(echo 'Searching $ARG ...' 1>&2 ; set -x; command luarocks \$CMD \$OPTS \$ARG${FILTER:+ | sed -e '$FILTER'}) || exit \$?"
   done)
 }
